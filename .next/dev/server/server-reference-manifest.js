f25558b570e58e887025a58d5f2faed7c3288b75
Ethicdev07 <akinadejunior76@gmail.com>
2026-08-23 09:55:44 +0100

    Remove Next.js build files from repository

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IUFXf8NUfeHf9ExJlGJuaoxOHiN4YRPGKlJzq0RLkIs=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SVPnCJ59GndvTd2OYMWPVIDt3tQPbytTPU19sKeLxSY=\"\n}"
